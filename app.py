040a25e6d526ab27f56b53dd36ab20909b2d804f
Carlos José Ortiz Bonill <carlosortiz@MacBook-Air-de-Carlos.local>
2024-02-15 17:51:13 -0500

    new commit

--- app.py
+++ app.py
@@ -1,12 +1,18 @@
 import streamlit as st
 import numpy as np
-from scipy.stats import t, norm, f
+from scipy.stats import t, norm, f, ttest_ind, levene
 import matplotlib.pyplot as plt
+import wooldridge as wd
+import seaborn as sns
+
+data = wd.data('wage1')[['wage', 'educ', 'exper', 'female']]
 
 st.title("Estadística inferencial")
 
-tab1, tab2 = st.tabs(["Intervalos de confianza",
-                      "Pruebas de hipótesis"])
+tab1, tab2, tab3, tab4 = st.tabs(["Intervalos de confianza",
+                            "Pruebas de hipótesis",
+                            "Regresión lineal",
+                            "Un pequeñísimo ejemplo"])
 
 with tab1:
     # generar una distribución t
@@ -149,4 +155,119 @@ with tab2:
     st.latex(r"t<-t_\alpha \hspace{0.5cm}\text{cola inferior}")
     st.latex(r"|t|>t_{\alpha/2} \hspace{0.5cm}\text{dos colas}")
 
- 
+with tab3:
+
+    x = np.linspace(0, 100, 10000)
+    e = np.random.normal(0, 1, 10000)
+    y = 7 + 10.4 * x + e
+
+    st.subheader("El modelo")
+
+    st.latex(r"y=\beta_0+\beta_1x+ u")
+    st.latex(r"y_i=\beta_0+\beta_1x_i+ u_i")
+
+    st.subheader("El mejor intercepto y la mejor pendiente")
+
+    st.latex(r"\hat{\beta}_0=\bar{y} -\hat{\beta}_1 \bar{x}")
+    st.latex(r"\hat{\beta}_1=\frac{\sum_{i=1}^n(x_i-\bar{x})(y_i-\bar{y})}{\sum_{i=1}^n (x_i-\bar{x})^2}")
+
+with tab4: 
+    st.header("EDA")
+
+    st.dataframe(data.head())
+
+    st.subheader("Métricas")
+
+    col1, col2, col3 = st.columns(3)
+
+    with col1:
+        st.metric(label="Media wage", value=str(round(data['wage'].mean(), 1)))
+    with col2:
+        st.metric(label="Media educ", value=str(round(data['educ'].mean(), 1)))
+    with col3:
+        st.metric(label="Media exper", value=str(round(data['exper'].mean(), 1)))
+
+    col1, col2, col3 = st.columns(3)
+
+    with col1:
+        st.metric(label="Mediana wage", value=str(round(data['wage'].median(), 1)))
+    with col2:
+        st.metric(label="Mediana educ", value=str(round(data['educ'].median(), 1)))
+    with col3:
+        st.metric(label="Mediana exper", value=str(round(data['exper'].median(), 1)))
+
+    col1, col2, col3 = st.columns(3)
+
+    with col1:
+        st.metric(label="Varianza wage", value=str(round(data['wage'].median(), 1)))
+    with col2:
+        st.metric(label="Varianza educ", value=str(round(data['educ'].median(), 1)))
+    with col3:
+        st.metric(label="Varianza exper", value=str(round(data['exper'].median(), 1)))
+    
+
+
+
+
+    st.subheader("Visualización")
+
+    st.write('Univariada')
+
+    fig, axes = plt.subplots(2, 2, figsize=(20, 20))
+
+    sns.histplot(data, x='wage', hue='female', ax=axes[0, 0])
+    sns.histplot(data, x='educ', hue='female', ax=axes[0, 1])
+    sns.histplot(data, x='exper', hue='female', ax=axes[1, 0])
+    
+    freq = data['female'].value_counts()
+    sns.barplot(x=freq.index, y=freq.values, ax=axes[1, 1])
+
+    st.pyplot(fig)
+
+    st.write('Bivariada')
+
+    fig = sns.pairplot(data, hue='female')
+    st.pyplot(fig)
+
+
+
+    st.subheader("Inferencia")
+
+    st.write("Diferencia salarial por género")
+
+    col1, col2 = st.columns(2)
+    men = data[data['female'] == 0]['wage']
+    women = data[data['female'] == 1]['wage']
+    with col1:
+        st.metric(label="Media hombres", value=str(round(men.mean(), 1)))
+    with col2:
+        st.metric(label="Media mujeres", value=str(round(women.mean(), 1)))
+
+    stat, p = ttest_ind(men, women)
+    if p < 0.05:
+        st.write("Rechazamos la hipótesis nula")
+    else:
+        st.write("No hay evidencia suficiente para rechazar la hipótesis nula.")
+
+
+    st.subheader("Predicción")
+
+    y = data['wage'].values
+    x = data[['educ', 'exper', 'female']].values
+
+    X = np.c_[np.ones(len(x)), x]
+
+    betas = np.linalg.inv(X.T.dot(X)).dot(X.T.dot(y))
+    st.latex(fr"wage_i={round(betas[0],1)}+{round(betas[1],1)}educ_i+{round(betas[2],1)}exper_i+{round(betas[3],1)}female_i")
+
+    educ = st.slider("Años de educación", min_value=0, max_value=30)
+    exper = st.slider("Años de educación",min_value=0, max_value=60 )
+    female = st.selectbox("Sexo", [0, 1])
+
+    arr = np.array([[1, educ, exper, female]])
+
+    if st.button("Predecir"):
+
+        st.write(f"Salario esperado: {arr.dot(betas)[0]}")
+
+
